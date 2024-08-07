9f95ebec1b39a9987e0553f68b5c4349023113e1
gladysh <gladyshinbox@outlook.com>
2024-08-07 23:36:20 +0200

    Add solution for Problem 4 - Increasing Array

---
+++ Introductory Problems/increasing_array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+#define ll long long
+
+// [1,4,5,6,7,7]
+
+int main() {
+  int n;
+  cin >> n;
+
+  int mx=0;
+  ll ans=0;
+  for(int i=0; i<n; ++i) {
+    int x;
+    cin >> x;
+
+    mx=max(mx, n);
+    ans+=mx-x;
+  }
+  cout << ans;
+}
